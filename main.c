f254453e32b001a9bb9a518df268767bd56fe8db
varad-netizen2050 <1272261031.v@mitwpu.edu.in>
2026-09-02 16:58:19 +0530

    Add files via upload
    Wrote a program to calculate the area of a rectangle using inputs supplied by the user

---
+++ main.c
@@ -0,0 +1,17 @@
+//Write a program that calculates perimeter of rectangle
+#include <stdio.h>
+int main(void){
+    double length,breadth;
+    printf("Enter length: \n");
+    scanf("%lf",&length);
+    printf("Enter Breadth: \n");
+    scanf("%lf1",&breadth);
+    printf("The perimeter of rectangle is %.2f:\n",2*(length+breadth));
+    return 0;
+}
+
+
+
+
+
+
