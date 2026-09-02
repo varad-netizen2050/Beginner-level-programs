9167ce86fe75885797a4324df9d4a1acdedd3ee2
varad-netizen2050 <1272261031.v@mitwpu.edu.in>
2026-09-02 16:46:57 +0530

    Update great.c

--- great.c
+++ great.c
@@ -1,12 +1,12 @@
 //Write a program to find greatest of four numbers entered by user
 #include<stdio.h>
 int main(){
-    int a,b,c,d;
+    int a,b,c,d; //To read four number entered
     printf("Enter four numbers: ");
-    scanf("%d %d %d %d",&a,&b,&c,&d);
+    scanf("%d %d %d %d",&a,&b,&c,&d);//Receiving inputs of all numbers at once
     if(a>b&&a>c&&a>d){
 
-        printf("The greatest of all is %d",a);
+        printf("The greatest of all is %d",a);    //Using if else and > operator to solve the problem
     }
     else if(b>a&&b>c&&b>d){
 
