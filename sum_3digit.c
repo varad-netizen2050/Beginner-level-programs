36d005dae4731231c7b8d4f25cc7a077e8182808
Varad Dnyaneshwar Kajale <1272261031.v@mitwpu.edu.in>
2026-09-19 08:10:24 +0530

    Update sum_3digit.c
    If you want the sum of digits of a three digit number, you welcome to use this program or logic to do so..

--- sum_3digit.c
+++ sum_3digit.c
@@ -1,19 +1,19 @@
 //Write a program that receives 3 digit numbers and calculate the sum of its digits
 #include<stdio.h>
-#include<stdlib.h>
+#include<stdlib.h> //To use absolute function 
 int main() {
-	int num,units,hundreds,tens,sum;
-	printf("Enter a 3 digit number:\n");
-	scanf("%d",&num);
-	int temp=abs(num);
+	int num,units,hundreds,tens,sum;     //Declarations or creation of variables of integer data type
+	printf("Enter a 3 digit number:\n"); 
+	scanf("%d",&num);                    //Input
+	int temp=abs(num); //To handle negative numbers entered
 	if(temp>=1000||temp<=99) {
-		printf("Error:NOT a 3 digit number\n");
+		printf("Error:NOT a 3 digit number\n");    //Rejecting any number format other than 3 digits using a single if block
 		return 1;
 	}
-	units=temp%10;
-	hundreds=temp/100;
-	tens=(temp/10)-hundreds*10;
-	sum=units+tens+hundreds;
-	printf("The sum of the digits of the entered number is %d\n",sum);
+	units=temp%10; //Modulo operator will provide remainder which will be our units digit
+	hundreds=temp/100; //Division operator used to obtain hundreds digit as an integer 
+	tens=(temp/10)-hundreds*10; //Some made up logic using arithmetic operators
+	sum=units+tens+hundreds; //Final calculations
+	printf("The sum of the digits of the entered number is %d\n",sum); //Final Output: Sum of digits
 	return 0;
 }
